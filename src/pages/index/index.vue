50547c917933f04953a4ab0106ad43c1b9942df0
wfei <wfei@foxhis.com>
2026-03-26 18:15:32 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <web-view :src="targetUrl" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+
+const DEFAULT_URL = 'http://davon-ld.icu/'
+const targetUrl = ref(DEFAULT_URL)
+
+onLoad(() => {
+  let title = '蓝友圈'
+  const match = targetUrl.value.match(/^https?:\/\/([^/]+)/i)
+  if (match?.[1]) title = match[1]
+  uni.setNavigationBarTitle({ title })
+})
+</script>
+
+<style></style>
